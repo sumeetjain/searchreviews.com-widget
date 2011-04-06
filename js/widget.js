5f2267aabb6d7acb90bb6e1ed7422c5706f602c7
Sumeet Jain <sumeet@sumeetjain.com>
2011-04-05 18:26:22 -0700

    Use JS to create the links. This will help when we get the JSON API to query for number of reviews.

--- js/widget.js
+++ js/widget.js
@@ -1,42 +1,60 @@
-// Get a no-nonsense string of keywords
-function srGetKeywords(elementID){
-	var rawString = document.getElementById(elementID).innerHTML.toLowerCase(); // This needs to be innerText()
-	var cleanString = rawString.replace(/the/g, "");
-	return cleanString
-}
-
-// Main function
-function srResultsWindow(keywords, UID){	
-	// If the results have already been shown
-	if (document.getElementById('searchReviewsWidget_' + UID)){
-		document.getElementById('searchReviewsWidget_' + UID).style.display = 'block';
+function srCreateLink(elementID){
+	// ################
+	// INNER FUNCTIONS:
+	// ################
+	
+	// Get a no-nonsense string of keywords
+	function srGetKeywords(elementID){
+		var rawString = document.getElementById(elementID).innerHTML.toLowerCase(); // This needs to be innerText()
+		var cleanString = rawString.replace(/the/g, "");
+		return cleanString;
 	}
-	// Else the results are being shown for the first time
-	else{
-		// Create the container for the overlay
-		var resultsWindow = document.createElement('div');
-		resultsWindow.className = 'searchReviewsWidget';
-		resultsWindow.id = 'searchReviewsWidget_' + UID;
-		
-		// Create the iframe that contains the results
-		var resultsIframe = document.createElement('iframe');
-		// resultsIframe.src = 'http://searchreviews.com/customsearch.jsp?reviews=' + keywords;
-		resultsIframe.src = 'sr/results.html?' + keywords;
-		resultsIframe.scrolling = 'no';
-		resultsIframe.frameBorder = 0;
-		
-		// Create the Close Overlay link
-		function closeResults(){
-			resultsWindow.style.display = "none";
+	
+	// Show iframe window
+	function srResultsWindow(keywords, UID){	
+		// If the results have already been shown
+		if (document.getElementById('searchReviewsWidget_' + UID)){
+			document.getElementById('searchReviewsWidget_' + UID).style.display = 'block';
 		}
-		var resultsClose = document.createElement('a');
-		resultsClose.className = 'searchReviewsCloseLink';
-		resultsClose.innerHTML = "Close";
-		resultsClose.onclick = closeResults;
-		
-		// Add the above elements to the page
-		resultsWindow.appendChild(resultsClose);
-		resultsWindow.appendChild(resultsIframe);
-		document.getElementsByTagName('body')[0].appendChild(resultsWindow);
+		// Else the results are being shown for the first time
+		else{
+			// Create the container for the overlay
+			var resultsWindow = document.createElement('div');
+			resultsWindow.className = 'searchReviewsWidget';
+			resultsWindow.id = 'searchReviewsWidget_' + UID;
+
+			// Create the iframe that contains the results
+			var resultsIframe = document.createElement('iframe');
+			// resultsIframe.src = 'http://searchreviews.com/customsearch.jsp?reviews=' + keywords;
+			resultsIframe.src = 'sr/results.html?' + keywords;
+			resultsIframe.scrolling = 'no';
+			resultsIframe.frameBorder = 0;
+
+			// Create the Close Overlay link
+			function closeResults(){
+				resultsWindow.style.display = "none";
+			}
+			var resultsClose = document.createElement('a');
+			resultsClose.className = 'searchReviewsCloseLink';
+			resultsClose.innerHTML = "Close";
+			resultsClose.onclick = closeResults;
+
+			// Add the above elements to the page
+			resultsWindow.appendChild(resultsClose);
+			resultsWindow.appendChild(resultsIframe);
+			document.getElementsByTagName('body')[0].appendChild(resultsWindow);
+		}
 	}
+	
+	// Create a link to the reviews overlay
+	var resultsLink = document.createElement('a');
+	resultsLink.className = 'searchReviewsLink';
+	resultsLink.href = '#';
+	resultsLink.innerHTML = 'I Found ## reviews.';
+	resultsLink.onclick = function(){
+		srResultsWindow(srGetKeywords(elementID), elementID);
+		return false;
+	}
+	
+	document.getElementsByTagName('body')[0].appendChild(resultsLink);
 }

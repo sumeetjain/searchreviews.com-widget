6f1b1fda622884962a05acebae7f7c897af26a15
Sumeet Jain <sumeet@sumeetjain.com>
2011-04-19 11:52:31 -0700

    Domready

--- js/widget.js
+++ js/widget.js
@@ -1,4 +1,9 @@
 document.write('<link rel="stylesheet" href="http://sumeetjain.com/searchreviews.com-widget/css/widget.css" type="text/css" media="screen" title="SearchReviews.com" charset="utf-8">');
+document.write('<script type="text/javascript" charset="utf-8" src="http://sumeetjain.com/searchreviews.com-widget/js/domready.js"></script>');
+
+DomReady.ready(function() {
+	window.readyToGo = true;
+});
 
 // Developed by Robert Nyman, http://www.robertnyman.com
 // Code/licensing: http://code.google.com/p/getelementsbyclassname/
@@ -77,168 +82,170 @@ var getElementsByClassName = function (className, tag, elm){
 	return getElementsByClassName(className, tag, elm);
 };
 
-// Create the super-container
-if(!document.getElementById('searchReviewsWidgetContainer')){
-	var resultsContainer = document.createElement('div');
-	resultsContainer.id = 'searchReviewsWidgetContainer';
-	resultsContainer.style.display = "none";
-	
-	// Create a div for the eventual iframe
-	var resultsSubcontainer = document.createElement('div');
-	resultsSubcontainer.id = 'searchReviewsWidgetSubcontainer';
-	
-	// Create the Close Overlay link
-	function closeResults(){
-		document.getElementById('searchReviewsWidgetContainer').style.display = "none";
-	}
-	var resultsClose = document.createElement('a');
-	resultsClose.id = 'searchReviewsCloseLink';
-	resultsClose.innerHTML = "Close";
-	resultsClose.title = "Click to close this window."
-	resultsClose.onclick = closeResults;
-	
-	// Create the Expand link
-	function expandResultsWindow(){
-		window.location = "http://searchreviews.com";
-	}
-	var expandResults = document.createElement('a');
-	expandResults.id = 'searchReviewsExpandLink';
-	expandResults.innerHTML = "Click to expand this window.";
-	expandResults.title = "Expand this window."
-	expandResults.href = 'http://searchreviews.com';
-	expandResults.target = '_blank';
-	
-	// Create a div to contain the Expand/Close links
-	var linksContainer = document.createElement('div');
-	linksContainer.id = 'searchReviewsLinksContainer';
+if(window.readyToGo){
+	// Create the super-container
+	if(!document.getElementById('searchReviewsWidgetContainer')){
+		var resultsContainer = document.createElement('div');
+		resultsContainer.id = 'searchReviewsWidgetContainer';
+		resultsContainer.style.display = "none";
 
-	// Add the above elements to the page
-	linksContainer.appendChild(resultsClose);
-	linksContainer.appendChild(expandResults);
-	resultsContainer.appendChild(linksContainer);
-	resultsContainer.appendChild(resultsSubcontainer);
-	document.getElementsByTagName('body')[0].appendChild(resultsContainer);
-}
+		// Create a div for the eventual iframe
+		var resultsSubcontainer = document.createElement('div');
+		resultsSubcontainer.id = 'searchReviewsWidgetSubcontainer';
 
-function srCreateLink(resultsLink){
-	
-	// Get a no-nonsense string of keywords
-	function srGetKeywords(elementID){
-		// If there's no elementID provided, or if it's not found...
-		if (elementID == null || document.getElementById(elementID) == null){
-			// Try using the <h1>
-			if (document.getElementsByTagName('h1')[0]){
+		// Create the Close Overlay link
+		function closeResults(){
+			document.getElementById('searchReviewsWidgetContainer').style.display = "none";
+		}
+		var resultsClose = document.createElement('a');
+		resultsClose.id = 'searchReviewsCloseLink';
+		resultsClose.innerHTML = "Close";
+		resultsClose.title = "Click to close this window."
+		resultsClose.onclick = closeResults;
+
+		// Create the Expand link
+		function expandResultsWindow(){
+			window.location = "http://searchreviews.com";
+		}
+		var expandResults = document.createElement('a');
+		expandResults.id = 'searchReviewsExpandLink';
+		expandResults.innerHTML = "Click to expand this window.";
+		expandResults.title = "Expand this window."
+		expandResults.href = 'http://searchreviews.com';
+		expandResults.target = '_blank';
+
+		// Create a div to contain the Expand/Close links
+		var linksContainer = document.createElement('div');
+		linksContainer.id = 'searchReviewsLinksContainer';
+
+		// Add the above elements to the page
+		linksContainer.appendChild(resultsClose);
+		linksContainer.appendChild(expandResults);
+		resultsContainer.appendChild(linksContainer);
+		resultsContainer.appendChild(resultsSubcontainer);
+		document.getElementsByTagName('body')[0].appendChild(resultsContainer);
+	}
+
+	function srCreateLink(resultsLink){
+
+		// Get a no-nonsense string of keywords
+		function srGetKeywords(elementID){
+			// If there's no elementID provided, or if it's not found...
+			if (elementID == null || document.getElementById(elementID) == null){
+				// Try using the <h1>
+				if (document.getElementsByTagName('h1')[0]){
+					if (document.all){
+						var rawString = document.getElementsByTagName('h1')[0].innerText.toLowerCase();
+					}
+					else{
+						var rawString = document.getElementsByTagName('h1')[0].textContent.toLowerCase();
+					}
+				}
+				// Or try the <meta name="description" />
+				else{
+						var metaTags = document.getElementsByTagName('meta');
+						for(var i = metaTags.length - 1; i >= 0; --i){
+
+							if(metaTags[i].name == 'description'){
+								var rawString = metaTags[i].content;
+							}
+						}
+				}
+			}
+			// Hopefully there's an elementID, though:
+			else{
 				if (document.all){
-					var rawString = document.getElementsByTagName('h1')[0].innerText.toLowerCase();
+					var rawString = document.getElementById(elementID).innerText.toLowerCase();
 				}
 				else{
-					var rawString = document.getElementsByTagName('h1')[0].textContent.toLowerCase();
+					var rawString = document.getElementById(elementID).textContent.toLowerCase();
 				}
 			}
-			// Or try the <meta name="description" />
-			else{
-					var metaTags = document.getElementsByTagName('meta');
-					for(var i = metaTags.length - 1; i >= 0; --i){
-						
-						if(metaTags[i].name == 'description'){
-							var rawString = metaTags[i].content;
-						}
-					}
+
+			var cleanString = rawString.replace(/the/g, "").replace(/and/g, "").replace(/to/g, "").replace(/how/g, "").replace(/if/g, "").replace(/review/g, "");
+
+			if (typeof console == "undefined") {
+			    window.console = {
+			        log: function () {}
+			    };
 			}
+			console.log("Keywords: " + cleanString); // Remove for production
+
+			return cleanString;
 		}
-		// Hopefully there's an elementID, though:
-		else{
-			if (document.all){
-				var rawString = document.getElementById(elementID).innerText.toLowerCase();
-			}
-			else{
-				var rawString = document.getElementById(elementID).textContent.toLowerCase();
-			}
+
+		// Show iframe window
+		function srResultsWindow(keywords){	
+			// Create another container for the overlay
+			var resultsWindow = document.createElement('div');
+
+			// Create the iframe that contains the results
+			var resultsIframe = document.createElement('iframe');
+			resultsIframe.src = 'http://searchreviews.com/customsearch.jsp?reviews=' + keywords;
+			// resultsIframe.src = 'sr/results.html?' + keywords;
+			resultsIframe.scrolling = 'no';
+			resultsIframe.frameBorder = 0;
+
+			// Add the above element to the overlay
+			resultsWindow.appendChild(resultsIframe);
+			document.getElementById('searchReviewsWidgetSubcontainer').innerHTML = resultsWindow.innerHTML;
+
+			// Change the "Expand" link to go to the right deep link
+			var expandResults = document.getElementById('searchReviewsExpandLink');
+			expandResults.href = "http://searchreviews.com/search.jsp?reviews=" + keywords;
 		}
-		
-		var cleanString = rawString.replace(/the/g, "").replace(/and/g, "").replace(/to/g, "").replace(/how/g, "").replace(/if/g, "").replace(/review/g, "");
-		
-		if (typeof console == "undefined") {
-		    window.console = {
-		        log: function () {}
-		    };
-		}
-		console.log("Keywords: " + cleanString); // Remove for production
-		
-		return cleanString;
-	}
-	
-	// Show iframe window
-	function srResultsWindow(keywords){	
-		// Create another container for the overlay
-		var resultsWindow = document.createElement('div');
 
-		// Create the iframe that contains the results
-		var resultsIframe = document.createElement('iframe');
-		resultsIframe.src = 'http://searchreviews.com/customsearch.jsp?reviews=' + keywords;
-		// resultsIframe.src = 'sr/results.html?' + keywords;
-		resultsIframe.scrolling = 'no';
-		resultsIframe.frameBorder = 0;
+		// Get reviewCount and update the link
+		var request = window.XDomainRequest ? new window.XDomainRequest() : new XMLHttpRequest();
+		var url = 'http://searchreviews.com/api/badge?reviews=' + srGetKeywords(resultsLink.rel) + '&time=' + new Date().getTime();
 
-		// Add the above element to the overlay
-		resultsWindow.appendChild(resultsIframe);
-		document.getElementById('searchReviewsWidgetSubcontainer').innerHTML = resultsWindow.innerHTML;
-		
-		// Change the "Expand" link to go to the right deep link
-		var expandResults = document.getElementById('searchReviewsExpandLink');
-		expandResults.href = "http://searchreviews.com/search.jsp?reviews=" + keywords;
-	}
-	
-	// Get reviewCount and update the link
-	var request = window.XDomainRequest ? new window.XDomainRequest() : new XMLHttpRequest();
-	var url = 'http://searchreviews.com/api/badge?reviews=' + srGetKeywords(resultsLink.rel) + '&time=' + new Date().getTime();
-
-	if(window.XDomainRequest){
-		request.onload = callback;
-		request.open("GET", url, true);
-	}
-	else{
-		request.open('GET', url, true);
-		request.onreadystatechange = callbackCheck;
-	}
-	request.send();
-
-	function callbackCheck(){
-		if (request.readyState == 4){
-			if (request.status == 200){
-				callback();
-			}
-			else{
-				alert("Error: request.readyState=" + request.readyState + ", request.status=" + request.status);
-			}
-		}
-	}
-
-	function callback(){
-		srJSON = eval('(' + request.responseText + ')');
-		if (srJSON.reviewCount > 0){
-			resultsLink.innerHTML = 'Found ' + srJSON.reviewCount + ' reviews.';
-			resultsLink.onclick = function(){
-				srResultsWindow(srGetKeywords(resultsLink.rel));
-				document.getElementById('searchReviewsWidgetContainer').style.display = "block";
-				return false;
-			}
+		if(window.XDomainRequest){
+			request.onload = callback;
+			request.open("GET", url, true);
 		}
 		else{
-			resultsLink.innerHTML = 'Search for reviews.';
-			resultsLink.onclick = function(){
-				window.location = "http://searchreviews.com";
+			request.open('GET', url, true);
+			request.onreadystatechange = callbackCheck;
+		}
+		request.send();
+
+		function callbackCheck(){
+			if (request.readyState == 4){
+				if (request.status == 200){
+					callback();
+				}
+				else{
+					alert("Error: request.readyState=" + request.readyState + ", request.status=" + request.status);
+				}
+			}
+		}
+
+		function callback(){
+			srJSON = eval('(' + request.responseText + ')');
+			if (srJSON.reviewCount > 0){
+				resultsLink.innerHTML = 'Found ' + srJSON.reviewCount + ' reviews.';
+				resultsLink.onclick = function(){
+					srResultsWindow(srGetKeywords(resultsLink.rel));
+					document.getElementById('searchReviewsWidgetContainer').style.display = "block";
+					return false;
+				}
+			}
+			else{
+				resultsLink.innerHTML = 'Search for reviews.';
+				resultsLink.onclick = function(){
+					window.location = "http://searchreviews.com";
+				}
 			}
 		}
 	}
-}
 
-// Find SearchReview Widget links and add widget-display functionality to them
-var allSearchReviewsLinks = getElementsByClassName('searchReviewsLink');
+	// Find SearchReview Widget links and add widget-display functionality to them
+	var allSearchReviewsLinks = getElementsByClassName('searchReviewsLink');
 
-for (var i = allSearchReviewsLinks.length - 1; i >= 0; --i){
-	allSearchReviewsLinks[i].onclick = function(){
-		return false;
+	for (var i = allSearchReviewsLinks.length - 1; i >= 0; --i){
+		allSearchReviewsLinks[i].onclick = function(){
+			return false;
+		}
+		srCreateLink(allSearchReviewsLinks[i]);
 	}
-	srCreateLink(allSearchReviewsLinks[i]);
 }
